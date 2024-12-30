4bcc48be4bdd9500a25375de9ce209d18062bef5
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-12-30 21:52:02 +0530

    console logged ipAddress of server on start.

--- WebSite/Backend/app.js
+++ WebSite/Backend/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import ip from "ip";
 
 const app = express();
 const port = process.env.PORT || 3000;
+const ipAddress = ip.address();
 
 app.get('/', (req, res) => {
     console.log(`Yoi, someone just pinged at ${new Date()}`);
@@ -9,5 +11,6 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+    console.log(`Server running on port http://localhost:${port}`);
+    console.log(`Network access at : http://${ipAddress}:${port}`);
 })

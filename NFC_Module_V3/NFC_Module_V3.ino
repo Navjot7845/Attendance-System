b2670c0bf0abaf9374170b7a9422d670beb82db4
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-12-31 16:04:24 +0530

    ESP 32 serial connection andRX & TX pins changed

--- NFC_Module_V3/NFC_Module_V3.ino
+++ NFC_Module_V3/NFC_Module_V3.ino
@@ -1,44 +1,42 @@
 #include <Wire.h>
 #include <Adafruit_PN532.h>
+#include <SoftwareSerial.h>
 
-// Define SDA and SCL pins for I2C communication
 #define SDA_PIN 2
 #define SCL_PIN 3
+#define RX_PIN 5
+#define TX_PIN 6
 
+SoftwareSerial esp32Serial(RX_PIN, TX_PIN);
 Adafruit_PN532 nfc(SDA_PIN, SCL_PIN);
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
+  esp32Serial.begin(9600);
   Serial.println("Initializing NFC...");
 
   nfc.begin();
-
-  // Check if the NFC module is found
   if (!nfc.getFirmwareVersion()) {
     Serial.println("PN532 NFC module not found!");
-    while (1); // Halt the program
+    while (1);
   }
-
-  nfc.SAMConfig(); // Configure the module to read passive targets
+  nfc.SAMConfig();
   Serial.println("NFC initialized. Waiting for a card...");
 }
 
 void loop() {
-  uint8_t uid[7];    // Buffer to hold the UID
-  uint8_t uidLength; // Length of the UID
+  uint8_t uid[7];
+  uint8_t uidLength;
 
-  // Try to read an NFC card
   if (nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength)) {
     Serial.println("Card detected!");
-    
-    // Print the UID
-    Serial.print("UID: ");
+    String uidString = "";
     for (uint8_t i = 0; i < uidLength; i++) {
-      Serial.print(uid[i], HEX);
-      Serial.print(" ");
+      if (uid[i] < 0x10) uidString += "0";
+      uidString += String(uid[i], HEX);
     }
-    Serial.println();
-
-    delay(1000); // Wait before scanning again
+    esp32Serial.println(uidString);
+    Serial.println("UID: " + uidString);
+    delay(3000);
   }
 }

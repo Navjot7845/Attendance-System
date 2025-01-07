b1004883d96509c422c953bf8b9ba41e5dbaeec6
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2025-01-07 22:02:26 +0530

    Verification code logics fixed

--- WebSite/Backend/app.js
+++ WebSite/Backend/app.js
@@ -1,12 +1,14 @@
 import express from 'express';
+import { spawn } from "child_process";
 import ip from "ip";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const ipAddress = ip.address();
 
 // Middleware to parse JSON payloads
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.post('/', (req, res) => {
     const { uid } = req.body; // Extract uid from the JSON payload
@@ -17,6 +19,24 @@ app.post('/', (req, res) => {
 
     console.log(`Received UID: ${uid}`);
     console.log(`Yoi, someone just pinged at ${new Date()}`);
+
+    const pythonProcess = spawn("python3", ["face-recognition/main.py"]);
+
+
+    pythonProcess.stdout.on('data', (name) => {
+        if (uid == "219c7726" && name == "Manik") {
+            console.log("Verified");
+        } else {
+            console.log("Fake");
+        }
+
+        console.log(`stdout: ${name}`);
+    });      
+
+    // pythonProcess.stderr.on('data', (data) => {
+    //     console.error(`stderr: ${data}`);
+    // });
+
     res.json({ message: "Yoi, someone just pinged", uid });
 });
 
